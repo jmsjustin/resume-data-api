b5de04ef17ec0ce2d177b46cd3aee38aa0f2552f
Justin Smith <jmsjustin@gmail.com>
2024-04-16 22:42:06 -0400

    added create action

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -8,4 +8,22 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Student.count, data.length
   end
+  test "create" do
+    assert_difference "Student.count", 1 do
+      post "/students.json", params: {
+                               first_name: "Hank",
+                               last_name: "Hankson",
+                               email: "hank@example.com",
+                               phone_number: "(234) 555-1234",
+                               short_bio: "Hank has fun. Hank is fun.",
+                               linkedin_url: "linkedin.com/hankhankson",
+                               twitter_handle: "x.com/HankTheTank",
+                               website_url: "www.TheRealHankson.com",
+                               resume_url: "resume.com/hirehank",
+                               github_url: "github.com/HHankson",
+                               photo: "jpeg.png",
+                             }
+      assert_response 200
+    end
+  end
 end
