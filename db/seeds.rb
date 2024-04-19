5b74023f418142682a3b0369413246d2874d1cbc
Justin Smith <jmsjustin@gmail.com>
2024-04-18 22:02:17 -0400

    fixed date format

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Student.create!(first_name: "Hank", last_name: "Hankson", email: "hank@example.c
 
 Capstone.create!(name: "My Capstone Project", description: "This is my capstone project", url: "capstone.com", screenshot: "capstone")
 
-Education.create!(start_date: 8 / 15 / 2019, end_date: 05 / 20 / 2023, degree: "Yes", university_name: "UNLV", details: "Hank the tank needs a job", created_at: 06 / 20 / 2023, updated_at: 04 / 18 / 2024)
+Education.create!(start_date: "8 / 15 / 2019", end_date: "05 / 20 / 2023", degree: "Yes", university_name: "UNLV", details: "Hank the tank needs a job", created_at: "06 / 20 / 2023", updated_at: "04 / 18 / 2024")
 Student.create!(first_name: "Reginald", last_name: "Hornswaggle", email: "reggie@horn.com", phone_number: "(423) 555-4689", short_bio: "Reggies horn can pierce the sky!", linkedin_url: "linkedin.com/reggiehorn", twitter_handle: "elonmusk", website_url: "www.VegWithReg.com", resume_url: "resume.com/hirereggie", github_url: "github.com/RHornswaggle", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtQ3zUCbRXw39Hp99c7NE4d6v6aXsCpz1UiA&s")
 
 Student.create!(first_name: "Concerned", last_name: "Ape", email: "concernedape@example.com", phone_number: "111-222-3333", short_bio: "creator of Stardew Valley -- working on Haunted Chocolatier", linkedin_url: "linkedin.com/concernedape", twitter_handle: "ConcernedApe", website_url: "https://www.stardewvalley.net/", resume_url: "resume.com/test", github_url: "github.com/test", photo: "https://pbs.twimg.com/profile_images/703728939995308032/IBU6i2IG_400x400.jpg")
